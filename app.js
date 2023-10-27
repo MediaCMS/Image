24cd2829aca7ff38ad99a4c9b2e7c16aa31b2391
Artem Vysotsky <a.vysotsky@gmail.com>
2023-10-27 20:10:59 +0300

    rem fetch check access

--- app.js
+++ app.js
@@ -17,20 +17,18 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(config.public));
 
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1);
+}
+
+app.get('/:slug', image.fetch);
 app.use(function(request, response, next) {
-    //console.log(request.path, request.params, request.body, request.headers)
     if (request.headers['x-api-key'] !== config.key) { 
         //log(`HTTP 403 Forbidden (${request.originalUrl})`);
         return response.sendStatus(403);
     }
     next();
 });
-
-if (app.get('env') === 'production') {
-    app.set('trust proxy', 1);
-}
-
-app.get('/:slug', image.fetch);
 app.post('/:slug', image.save);
 app.patch('/:slug', image.rename);
 app.delete('/:slug', image.remove);
