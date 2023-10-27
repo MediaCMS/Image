5a736aac501f13b771a34a81be6d06173b7e9858
Artem Vysotsky <a.vysotsky@gmail.com>
2023-10-27 20:15:52 +0300

    ref benchmark

--- benchmark/script.lua
+++ benchmark/script.lua
@@ -1,21 +1,19 @@
 
-randomseed = tonumber(tostring(math.ceil(os.time() / os.clock())):reverse())
+randomseed = tonumber(
+  tostring(
+    math.ceil(os.time() / os.clock())
+  ):reverse()
+)
 math.randomseed(randomseed)
 math.random(); math.random(); math.random()
 
 paths = {}
-for path in io.lines('paths3.txt') do
+for path in io.lines('urls.txt') do
   paths[#paths + 1] = path
 end
-
---print('Count of paths: ' .. #paths)
 
 request = function()
   local index = math.random(#paths)
   local path = paths[index]
-  local headers = {
-    ['x-api-key'] = '9e7f1D53edf0a5E9b259b3e679C65ec6'
-  }
-  --print('path ' .. path .. ' [' .. index .. '] ' .. headers['x-api-key'])
-  return wrk.format('GET', path, headers)
+  return wrk.format('GET', path)
 end
